f2853f0ab85565d4c908053fbe7860b6097ce0da
ihsgnef <shifeng@cs.umd.edu>
2018-06-29 18:04:47 +0000

    dbpedia fix

--- text_datasets.py
+++ text_datasets.py
@@ -29,6 +29,7 @@ def download_dbpedia():
 def read_dbpedia(tf, split, shrink=1, char_based=False):
     dataset = []
     f = tf.extractfile('dbpedia_csv/{}.csv'.format(split))
+    f = io.StringIO(f.read().decode('utf-8'))
     for i, (label, title, text) in enumerate(csv.reader(f)):
         if i % shrink != 0:
             continue

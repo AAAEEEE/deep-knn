6adb4fcd9615ed4534ccf06f9fadfb09b7fdbd6b
Eric Wallace <eswallace@comcast.net>
2018-07-04 14:33:00 -0700

    word colorize

--- leave_one_out.py
+++ leave_one_out.py
@@ -6,7 +6,11 @@ from tqdm import tqdm, tqdm_notebook
 from collections import Counter
 
 import numpy as np
-import cupy as cp
+#import cupy as cp
+
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 import chainer
 import chainer.functions as F
@@ -21,8 +25,8 @@ def flatten(x):
     '''generate version of x with each token removed'''
     assert x.ndim == 1
     assert x.shape[0] > 1
-    if cp.get_array_module(x) == cp:
-        x = cp.asnumpy(x)
+    # if cp.get_array_module(x) == cp:
+    #     x = cp.asnumpy(x)
     xs = []
     for i in range(x.shape[0]):
         xs.append(np.concatenate((x[:i], x[i+1:]), axis=0))
@@ -33,22 +37,16 @@ def leave_one_out(x, y, scorer, gpu=True):
     # flatten
     assert x.ndim == 1
 
-    if cp.get_array_module(x) == cp:
-        x = cp.asnumpy(x)
+    # if cp.get_array_module(x) == cp:
+    #     x = cp.asnumpy(x)
     xs = flatten(x)
-    if gpu:
-        xs = [cp.asarray(x) for x in xs]
+    # if gpu:
+    #     xs = [cp.asarray(x) for x in xs]
     ys = [y for _ in xs]
 
     # rank
     scores = scorer(xs, ys)
     return scores
-
-
-
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 
 def colorize(words, color_array):
     # words is a list of words
@@ -88,34 +86,46 @@ def main():
     dknn.calibrate(train[:1000], batch_size=setup['batchsize'],
                    converter=converter, device=setup['gpu'])
 
-    for i in range(100):
+    for i in range(20): 
         label = int(test[i][1])
         text = test[i][0]
-        y, original_score, _, _, _ = dknn.predict([cp.asarray(text)])
+        y, original_score, _, _, _ = dknn.predict([text])
         x = text
         y = y[0]
         scores = leave_one_out(x, y, dknn.get_credibility)
-        scores = sorted(list(enumerate(scores)), key=lambda x: x[1])
-        
-
-        # words = 'The quick brown fox jumps over the lazy dog'.split()
-        # color_array = np.random.rand(len(words))
-        # s = colorize(words, color_array)
-        print(' '.join(reverse_vocab[w] for w in x))
-        print('label: {}'.format(label))
-        print('prediction: {} ({})'.format(y, original_score[0]))          
-        for idx, score in scores[:]:
-            if score < 1.0:
-                print(score, reverse_vocab[x[idx]])            
+        scores = list(enumerate(scores))
+        sorted_scores = sorted(scores, key=lambda x: x[1])                          
         
         # visualize results in heatmap
-        words = [reverse_vocab[w] for w in x]
-        visual = colorize(words, scores)
-        with open('colorize.html', 'w') as f:
-            f.write(s)
+        normalized_scores = []
+        words = []
+        for idx, score in scores[:]:
+            normalized_scores.append(score - original_score[0])            
+            words.append(reverse_vocab[x[idx]])
+
+        total_score = np.sum(normalized_scores)
+        normalized_scores = [0.5 + n / total_score for n in normalized_scores]
+
+        visual = colorize(words, normalized_scores)
+        with open('colorize.html', 'a') as f:
+            if y == 1:
+                f.write("Positive ({})         ".format(original_score[0]))
+
+                print(' '.join(reverse_vocab[w] for w in x))
+                print('label: {}'.format(label))
+                print('prediction: {} ({})'.format(y, original_score[0]))          
+                for idx, score in sorted_scores[:]:
+                    if score < 1.0:
+                        print(score, reverse_vocab[x[idx]])
+
+            else:
+                continue
+                f.write("Negative ({})         ".format(original_score[0]))
+
+            f.write(visual + "<br>")
 
         # print neighbors
-        neighbors = dknn.get_neighbors([cp.asarray(text)])
+        neighbors = dknn.get_neighbors([text])
         print('neighbors:')
         for neighbor in neighbors[:5]:
             curr_nearest_neighbor_input_sentence = '     '

9743f8dd00b7810c9e296e1f33e7871780710e47
ihsgnef <shifeng@cs.umd.edu>
2018-07-05 22:25:34 +0000

    leave one out with vanilla gradient

--- l1o.py
+++ l1o.py
@@ -2,11 +2,16 @@
 import argparse
 import numpy as np
 import cupy as cp
-
+import warnings
+from functools import partial
 import matplotlib
+import matplotlib.pyplot as plt
+
+import chainer
+import chainer.functions as F
 
 from nlp_utils import convert_seq, convert_snli_seq
-from util import setup_model
+from utils import setup_model
 from run_dknn import DkNN
 
 
@@ -45,9 +50,12 @@ def flatten(x):
     return xs
 
 
-def leave_one_out(x, y, scorer, gpu=True, bigrams=False, snli=False):
-    if cp.get_array_module(x) == cp:
-        x = cp.asnumpy(x)
+def leave_one_out(dknn, x, bigrams=False, snli=False):
+    ys, original_score, _, _, _ = dknn.predict([x], snli=snli)
+    y = ys[0]
+    original_score = original_score[0]
+    gpu = cp.get_array_module(x) == cp
+    x = cp.asnumpy(x)
     if snli:
         xs = snli_flatten(x)
     elif bigrams:
@@ -56,17 +64,32 @@ def leave_one_out(x, y, scorer, gpu=True, bigrams=False, snli=False):
         xs = flatten(x)
     if gpu:
         xs = [cp.asarray(x) for x in xs]
-    ys = [y for _ in xs]
+    ys = [int(y) for _ in xs]
 
     # rank
-    scores = scorer(xs, ys)
-    return scores
+    scores = dknn.get_credibility(xs, ys)
+    # scores = dknn.get_regular_confidence(xs, ys)
+    return y, original_score, scores
+
+
+def vanilla_grad(model, x, bigrams=False, snli=False):
+    if bigrams:
+        warnings.warn('bigrams not supported for vanilla grad')
+    if snli:
+        warnings.warn('snli not supported for vanilla grad')
+    with chainer.using_config('train', False):
+        output = model.predict([x], softmax=True)
+        y = int(F.argmax(output).data)
+        original_score = float(F.max(output).data)
+    onehot_grad = model.get_onehot_grad([x])[0].data
+    onehot_grad = cp.asnumpy(onehot_grad).tolist()
+    return y, original_score, onehot_grad
 
 
 def colorize(words, color_array, colors='RdBu'):
     # words is a list of words
     # color_array is an array of numbers between 0 and 1
-    cmap = matplotlib.cm.get_cmap(colors)
+    cmap = plt.cm.get_cmap(colors)
     template = '<span class="barcode"; style="color: black; \
                 background-color: {}">{}</span>'
     colored_string = ''
@@ -103,39 +126,39 @@ def main():
 
     dknn = DkNN(model, args.lsh)
     dknn.build(train, batch_size=setup['batchsize'],
-               converter=converter, device=setup['gpu'])
+               converter=converter, device=args.gpu)
 
     # need to select calibration data more carefully
     dknn.calibrate(train[:1000], batch_size=setup['batchsize'],
-                   converter=converter, device=setup['gpu'])
+                   converter=converter, device=args.gpu)
+
+    ranker = partial(leave_one_out, dknn)
+    # ranker = partial(vanilla_grad, model)
 
     for i in range(len(test)):
         if use_snli:
+            # FIXME
             label = int(test[i][2])
-            text = ([test[i][0]], [test[i][1]])
-            x = text
+            x = ([test[i][0]], [test[i][1]])
         else:
-            label = int(test[i][1])
-            text = [test[i][0]]
-            x = text[0]
-        y, original_score, _, _, _ = dknn.predict(cp.asarray(text), snli=use_snli)
+            text, label = test[i]
+            x = cp.asarray(text)
+            y = cp.asarray(label)
 
-        y = y[0]
-        scores = leave_one_out(x, y, dknn.get_credibility, snli=use_snli)
-        # scores = leave_one_out(x, y, dknn.get_regular_confidence, snli=use_snli)
-        scores = list(enumerate(scores))
-        sorted_scores = sorted(scores, key=lambda x: x[1])
+        prediction, original_score, scores = ranker(x, snli=use_snli)
+        sorted_scores = sorted(list(enumerate(scores)), key=lambda x: x[1])
 
-        print(' '.join(reverse_vocab[w] for w in x))
-        print('label: {}'.format(label))
-        print('prediction: {} ({})'.format(y, original_score[0]))
-        for idx, score in sorted_scores[:]:
-            if score < original_score[0]:
-                print(score, reverse_vocab[x[idx]])
+        print(' '.join(reverse_vocab[w] for w in text))
+        print('label: {}'.format(label[0]))
+        print('prediction: {} ({})'.format(prediction, original_score))
+
+        for idx, score in sorted_scores:
+            if score < original_score:
+                print(score, reverse_vocab[text[idx]])
 
         # print()
         # # bigrams
-        # scores = leave_one_out(x, y, dknn.get_credibility, bigrams = True)
+        # scores = ranker(x, y, bigrams=True)
         # scores = list(enumerate(scores))
         # sorted_scores = sorted(scores, key=lambda x: x[1])
         # bigrams = [b for l in text for b in zip(x[:-1], x[1:])]
@@ -143,14 +166,13 @@ def main():
         #     if score < 1.0:
         #         print(score, reverse_vocab[bigrams[idx][0]] + ' ' + reverse_vocab[bigrams[idx][1]])
 
-
         # plot sentiment results visualize results in heatmap
         normalized_scores = []
         words = []
-        for idx, score in scores[:]:
-            normalized_scores.append(score - original_score[0])
-            words.append(reverse_vocab[x[idx]])
-        if y == 1:  # flip sign if positive
+        for idx, score in enumerate(scores):
+            normalized_scores.append(score - original_score)
+            words.append(reverse_vocab[text[idx]])
+        if prediction == 1:  # flip sign if positive
             normalized_scores = [-1 * n for n in normalized_scores]
 
         normalized_scores = [0.5 + n for n in normalized_scores]
@@ -161,16 +183,16 @@ def main():
             else:
                 f.write('Ground Truth Label: Negative')
             if y == 1:
-                f.write("Prediction: Positive ({})         ".format(original_score[0]))
+                f.write("Prediction: Positive ({})         ".format(original_score))
             else:
-                f.write("Prediction: Negative ({})         ".format(original_score[0]))
+                f.write("Prediction: Negative ({})         ".format(original_score))
             f.write(visual + "<br>")
 
         # plot snli results
         # normalized_scores = []
         # words = []
         # for idx, score in scores[:]:
-        #     normalized_scores.append(score - original_score[0])
+        #     normalized_scores.append(score - original_score)
         #     words.append(reverse_vocab[x[idx]])
         # normalized_scores = [0.5 + n for n in normalized_scores]
         # visual = colorize(words, normalized_scores, colors = colors)
@@ -185,11 +207,11 @@ def main():
         #         exit("Label not found")
 
         #     if y == 2:
-        #         f.write("Prediction: Entailment ({})         ".format(original_score[0]))
+        #         f.write("Prediction: Entailment ({})         ".format(original_score))
         #     elif y == 1:
-        #         f.write("Prediction: Neutral ({})         ".format(original_score[0]))
+        #         f.write("Prediction: Neutral ({})         ".format(original_score))
         #     elif y == 0:
-        #         f.write("Prediction: Contradiction ({})         ".format(original_score[0]))
+        #         f.write("Prediction: Contradiction ({})         ".format(original_score))
         #     else:
         #         eixt("Prediction Label not found")
         #     f.write(visual + "<br>")
@@ -197,7 +219,7 @@ def main():
         # display scores normalized across words
         # normalized_scores = []
         # for idx, score in scores[:]:
-        #     normalized_scores.append(score - original_score[0])
+        #     normalized_scores.append(score - original_score)
         # if y == 1:  # flip sign if positive
         #     normalized_scores = [-1 * n for n in normalized_scores]
         # total_score = 1e-6
@@ -211,14 +233,13 @@ def main():
         #     else:
         #         f.write('Ground Truth Label: Negative&nbsp;')
         #     if y == 1:
-        #         f.write("Prediction: Positive ({})         ".format(original_score[0]))
+        #         f.write("Prediction: Positive ({})         ".format(original_score))
         #     else:
-        #         f.write("Prediction: Negative ({})         ".format(original_score[0]))
+        #         f.write("Prediction: Negative ({})         ".format(original_score))
         #     f.write(visual + "<br>")
 
-
             # print neighbors
-            neighbors = dknn.get_neighbors(cp.asarray(text))
+            neighbors = dknn.get_neighbors([x])
             print('neighbors:')
             f.write('&nbsp;&nbsp;&nbsp;&nbsp;Nearest Neighbor Sentences: <br>')
             for neighbor in neighbors[:5]:

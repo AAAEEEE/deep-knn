54924bc892a6ebe8bde3a416c674b4bae2732ba9
Eric Wallace <ewallace@lyft.com>
2018-07-11 14:47:00 -0700

    enable cupy

--- l1o.py
+++ l1o.py
@@ -2,7 +2,7 @@
 import pickle
 import argparse
 import numpy as np
-#import cupy as cp
+import cupy as cp
 import warnings
 from functools import partial
 import matplotlib
@@ -33,8 +33,8 @@ from run_dknn import DkNN
 
 def snli_flatten(x):
     '''generate version of x with each token removed'''
-    #if cp.get_array_module(x) == cp:
-     #   x = cp.asnumpy(x)
+    if cp.get_array_module(x) == cp:
+        x = cp.asnumpy(x)
     reduced_hypo = []
     prem = []    
     for i in range(len(x[1][0])):
@@ -49,8 +49,8 @@ def flatten(x):
     '''generate version of x with each token removed'''
     assert x.ndim == 1
     assert x.shape[0] > 1
-    #if cp.get_array_module(x) == cp:
-        #x = cp.asnumpy(x)
+    if cp.get_array_module(x) == cp:
+        x = cp.asnumpy(x)
     xs = []    
     for i in range(x.shape[0]):
         xs.append(np.concatenate((x[:i], x[i+1:]), axis=0))
@@ -69,8 +69,8 @@ def leave_one_out(dknn, x, bigrams=False, snli=False, use_credibility=True):
     else:
         y = reg_pred[0]    
 
-    #gpu = cp.get_array_module(x) == cp
-    #x = cp.asnumpy(x)
+    gpu = cp.get_array_module(x) == cp
+    x = cp.asnumpy(x)
     if snli:
         xs = snli_flatten(x)
         ys = [int(y) for _ in xs[0]]
@@ -80,8 +80,8 @@ def leave_one_out(dknn, x, bigrams=False, snli=False, use_credibility=True):
     else:
         xs = flatten(x)
         ys = [int(y) for _ in xs]
-    #if gpu:
-        #xs = [cp.asarray(x) for x in xs]
+    if gpu:
+        xs = [cp.asarray(x) for x in xs]
     
     # rank
     if use_credibility:
@@ -188,7 +188,7 @@ def main():
             x = ([prem], [hypo])                        
         else:
             text, label = test[i]
-            x = text#cp.asarray(text) #text   
+            x = cp.asarray(text) #text   
 
         if args.interp_method == 'dknn':
             use_cred = True
